1dfe719ea8c6439a7445867c4ef9a6851c06a4cb
Mansour <theboom.kajas@gmail.com>
2024-02-27 12:09:14 +0100

    Added the DetailActivity with CollapsingToolbar with the needed styles to make it work

---
+++ app/src/main/java/com/example/superheroes/activities/DetailActivity.kt
@@ -0,0 +1,100 @@
+package com.example.superheroes.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import com.example.superheroes.R
+import com.example.superheroes.data.Superhero
+import com.example.superheroes.data.SuperheroesServiceApi
+import com.example.superheroes.databinding.ActivityDetailBinding
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class DetailActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_ID = "SUPERHERO_ID"
+        const val EXTRA_NAME = "SUPERHERO_NAME"
+        const val EXTRA_IMAGE = "SUPERHERO_IMAGE"
+    }
+
+    private var superheroId:String? = null
+    private lateinit var superhero:Superhero
+
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initActionBar()
+
+        superheroId = intent.getStringExtra(EXTRA_ID)
+        val name = intent.getStringExtra(EXTRA_NAME)
+        val image = intent.getStringExtra(EXTRA_IMAGE)
+
+        binding.toolbarLayout.title = name
+        Picasso.get().load(image).into(binding.photoImageView)
+
+        findSuperheroById(superheroId!!)
+    }
+
+    private fun initActionBar() {
+        setSupportActionBar(binding.toolbar)
+
+        // Show back button
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+
+        //val collapsingToolbar: CollapsingToolbarLayout = findViewById(R.id.toolbar_layout)
+        //collapsingToolbar.title = "Your Title"
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun findSuperheroById(id: String) {
+        //binding.progress.visibility = View.VISIBLE
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://superheroapi.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service: SuperheroesServiceApi = retrofit.create(SuperheroesServiceApi::class.java)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            // Llamada en segundo plano
+            val response = service.findById(id)
+
+            runOnUiThread {
+                // Modificar UI
+                //binding.progress.visibility = View.GONE
+                if (response.body() != null) {
+                    Log.i("HTTP", "respuesta correcta :)")
+                    superhero = response.body()!!
+                    binding.toolbarLayout.title = superhero.name
+                } else {
+                    Log.i("HTTP", "respuesta erronea :(")
+                }
+            }
+        }
+    }
+}
